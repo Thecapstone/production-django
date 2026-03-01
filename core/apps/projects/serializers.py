e2761a30f4b99a5a312c5d466e3c9f24ebb64ca5
Thecapstone <thecapstonemogbolu@gmail.com>
2026-03-01 23:20:32 +0100

    updated project serializers for lists and update

--- core/apps/projects/serializers.py
+++ core/apps/projects/serializers.py
@@ -7,20 +7,31 @@ from core.utils.enums import ModeratorRoles
 
 class ProjectSerializer:
     class CreateProject(serializers.ModelSerializer):
-        title = serializers.SerializerMethodField()
-        moderators = serializers.ChoiceField(choices=ModeratorRoles.choices, required=False, default=Project.creator)
         creator = serializers.PrimaryKeyRelatedField(queryset=User.objects.all)
+        
+        class Meta:
+            model = Project
+            fields = ["title", "creator","about", "rules"]
+    
+        def validate_project_title(self, title):
+            """
+            ensure that a user cannot create a project with the same name as an existing project.
+            """
+            if Project.objects.filter(title__iexact=title).exists():
+                raise serializers.ValidationError("A project with this name already exists.")
+            
+    class ProjectList(serializers.ModelSerializer):
+        detail = serializers.SerializerMethodField()
 
-        def get_title(self, obj):
+        def get_detail(self, obj):
             return (f"{obj.title} project by {obj.creator}")
         
         class Meta:
             model = Project
-            fields = ["title", "creator", "about", "rules"]
-    
-    def validate_project_name(serializers.Serializer):
-        """
-        ensure that a user cannot create a project with the same name as an existing project.
-        """
-        if Project.objects.filter(title__iexact=serializers.validated_data["title"]).exists():
-            raise serializers.ValidationError("A project with this name already exists.")
+            fields = ('__all__', 'detail')
+
+        
+    class ProjectUpdateSerializer(serializers.ModelSerilizers):
+        model = Project
+        fields = ('title', "about", "rules")
+
