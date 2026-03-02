ac0e178b916adc6caaa01f0da1b2066575ea8c34
Thecapstone <thecapstonemogbolu@gmail.com>
2026-03-02 21:13:01 +0100

    made the 'about' method a property method

--- core/apps/projects/models.py
+++ core/apps/projects/models.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING, ClassVar
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from core.utils.models import UIDTimeBasedModel
-from core.utils.enums import ModeratorRoles
+from core.utils.enums import ModeratorRolesEnums
 from .managers import ProjectManager
 
 
@@ -15,11 +15,12 @@ class Project(UIDTimeBasedModel):
     title = models.CharField(max_length=255)
     creator = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="creator")
     about = models.TextField(_("Description of the Project"), blank=True)
-    moderators: models.QuerySet["Moderator"] = models.ForeignKey("users.Moderator", on_delete=models.CASCADE, choices=ModeratorRoles.choices, related_name="project_managers", blank=True)
+    moderators: models.QuerySet["Moderator"] = models.ForeignKey("users.Moderator", on_delete=models.CASCADE, choices=ModeratorRolesEnums.choices, related_name="project_managers", blank=True)
     members = models.ManyToManyField("users.User", related_name="project_members", blank=True)
     rules = models.TextField(_("Project Rules"), blank=True)
     objects: ClassVar[ProjectManager.Manager] = ProjectManager.Manager()
 
+    @property
     def about(self) -> str:
         return f""" {self.title} is a project created by {self.creator}, it has {(self.moderators).count()} managers, and {(self.members).count()} members
         Description: {self.about}
