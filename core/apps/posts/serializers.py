c6211195b20197bfbef51e8b74628f06e6c3e5c7
Thecapstone <thecapstonemogbolu@gmail.com>
2026-03-09 00:27:18 +0100

    created eager loading function for query optimization

--- core/apps/posts/serializers.py
+++ core/apps/posts/serializers.py
@@ -3,11 +3,29 @@ from . import managers
 from .models import IdeaThread
 from core.helpers.enums import CommentReportReason
 
+class EagerLoadingMixin:
+    @classmethod
+    def setup_eager_loading(cls, queryset):
+        if hasattr(cls, "SELECT"):
+            queryset = queryset.select_related(*cls.SELECT)
+        if hasattr(cls, "PREFETCH"):
+            queryset = queryset.prefetch_related(*cls.PREFETCH)
+
 class PostsSerializer:
     class CreateQuestionAndAnswerSerializer(serializers.ModelSerializer):
         class Meta:
             model = managers.QuestionAndAnswer
-            fields = ("title", "content", "choices", 'community', 'project')
+            fields = ("title", "content", "choices", 'tags', 'community', 'project')
+    
+    class CreateIdeaThreadSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = managers.QuestionAndAnswer
+            fields = ( "content", 'community','tags', 'project')
+    
+    class CreateLongDraftSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = managers.QuestionAndAnswer
+            fields = ("title", "content",'tags', 'community', 'project')
 
 
     class ReportPostSerializer(serializers.ModelSerializer):
